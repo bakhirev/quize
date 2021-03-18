74e8a692f5e6e8a0853d78e9ed91a1e8ea245277
Бахирев Алексей Михайлович (4044820) <abakhirev@vtb.ru>
2021-03-18 12:59:23 +0300

    fix code style

--- public/api/email.php
+++ public/api/email.php
@@ -1,20 +1,20 @@
 <?php
     $data = getJsonFromBody();
     $message = getTextBySchema($data, [
-		'name' => 'Имя',
-		'phone' => 'Телефон',
-		'home' => 'Тип объекта',
-		'cars' => 'Количество машиномест',
-		'exit-in' => 'Количество въездов на территорию парковки',
-		'exit-out' => 'Количество выездов с территории парковки',
-		'exit-reverse' => 'Количество реверсивных проездов (сквозной въезд-выезд)',
-		'users' => 'Пользователи парковки',
-		'terminals' => 'Сколько терминалов оплаты вам нужно',
-		'address' => 'Адрес объекта',
-	]);
+        'name' => 'Имя',
+        'phone' => 'Телефон',
+        'home' => 'Тип объекта',
+        'cars' => 'Количество машиномест',
+        'exit-in' => 'Количество въездов на территорию парковки',
+        'exit-out' => 'Количество выездов с территории парковки',
+        'exit-reverse' => 'Количество реверсивных проездов (сквозной въезд-выезд)',
+        'users' => 'Пользователи парковки',
+        'terminals' => 'Сколько терминалов оплаты вам нужно',
+        'address' => 'Адрес объекта',
+    ]);
     $status = sendEmail('lid@r-p-s.ru', $message);
     // sendEmail('alexey-bakhirev@yandex.ru', $message);
-	sendResponse($status ? 'ok' : 'error');
+    sendResponse($status ? 'ok' : 'error');
 
     function getJsonFromBody() {
         $rawInput = fopen('php://input', 'r');
@@ -27,7 +27,7 @@
 
     
     function sendEmail($email, $message) {
-		if (is_null($message)) return false;
+        if (is_null($message)) return false;
 
         $header  = "From: support@bakhirev.biz\n";
         $header .= "Content-type: text/html; charset=\"utf-8\"";
@@ -37,31 +37,31 @@
         $text .= "<META http-equiv=Content-Type content='text/html; charset=utf-8'>\r\n";
         $text .= "</HEAD>\r\n";
         $text .= "<BODY>\r\n";
-		$text .=  $message."\r\n";
+        $text .=  $message."\r\n";
         $text .= "</BODY>\r\n";
         $text .= "</HTML>";
 
 
         @mail($email, 'Заявка по quiz', $text ,$header);
-		return true;
+        return true;
     }
     
     function getTextBySchema($data, $schema) {
-		if (is_null($data)) return null;
-		$text = '';
-		foreach ($schema as $key => $value) {
-			if (!isset($data[$key])) continue;
-			if (is_array($data[$key])) {
-				$text .= $value . ': ' . implode(', ', array_map(htmlspecialchars, $data[$key])) . '<br>';
-			} else {
-				$text .= $value . ': ' . htmlspecialchars($data[$key]) . '<br>';
-			}
+        if (is_null($data)) return null;
+        $text = '';
+        foreach ($schema as $key => $value) {
+            if (!isset($data[$key])) continue;
+            if (is_array($data[$key])) {
+                $text .= $value . ': ' . implode(', ', array_map(htmlspecialchars, $data[$key])) . '<br>';
+            } else {
+                $text .= $value . ': ' . htmlspecialchars($data[$key]) . '<br>';
+            }
         }
-		return $text;
+        return $text;
     }
 
     function sendResponse($status) {
-		header($_SERVER["SERVER_PROTOCOL"] . " 200 ", true, $code);
+        header($_SERVER["SERVER_PROTOCOL"] . " 200 ", true, $code);
         header("Content-Type: application/json; charset=utf-8");
         echo "{\"status\":\"{$status}\"}";
     }
